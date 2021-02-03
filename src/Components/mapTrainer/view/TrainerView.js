52bfc76cf1e08b81020a66079fc874fd9f813757
Sakthivel <sakthivel.govindan@tarento.com>
2021-02-03 12:23:09 +0530

    Added function to get coordinates of bounding box

--- src/Components/mapTrainer/view/TrainerView.js
+++ src/Components/mapTrainer/view/TrainerView.js
@@ -46,19 +46,12 @@ export default function TrainerView(props) {
   const classes = useStyles();
 
   const [canvas, setCanvas] = useState("");
-  // const canvasRef = useRef(null)
-
-  // const initCanvas = () => {
-  //   new fabric.Canvas("canvas", {
-  //     height: 400,
-  //     width: 400,
-  //     backgroundColor: "white",
-  //   });
+  const [coord, setCoord] = useState("");
 
   const initCanvas = (props) => {
     let canvas = new fabric.Canvas("canvas", {});
 
-    let rectangle, isDown, origX, origY;
+    let rectangle, isDown, origX, origY, coordinates_info;
 
     let deleteIcon =
       "data:image/svg+xml,%3C%3Fxml version='1.0' encoding='utf-8'%3F%3E%3C!DOCTYPE svg PUBLIC '-//W3C//DTD SVG 1.1//EN' 'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'%3E%3Csvg version='1.1' id='Ebene_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' width='595.275px' height='595.275px' viewBox='200 215 230 470' xml:space='preserve'%3E%3Ccircle style='fill:%23F44336;' cx='299.76' cy='439.067' r='218.516'/%3E%3Cg%3E%3Crect x='267.162' y='307.978' transform='matrix(0.7071 -0.7071 0.7071 0.7071 -222.6202 340.6915)' style='fill:white;' width='65.545' height='262.18'/%3E%3Crect x='266.988' y='308.153' transform='matrix(0.7071 0.7071 -0.7071 0.7071 398.3889 -83.3116)' style='fill:white;' width='65.544' height='262.179'/%3E%3C/g%3E%3C/svg%3E";
@@ -68,16 +61,13 @@ export default function TrainerView(props) {
 
     canvas.on("mouse:down", function (o) {
       var pointer = canvas.getPointer(o.e);
-      console.log(isDown);
       if (!isDown) {
         isDown = true;
         origX = pointer.x;
         origY = pointer.y;
-
         fabric.Object.prototype.transparentCorners = false;
         fabric.Object.prototype.cornerColor = "red";
         fabric.Object.prototype.cornerStyle = "circle";
-
         rectangle = new fabric.Rect({
           left: origX,
           top: origY,
@@ -87,9 +77,16 @@ export default function TrainerView(props) {
           selectable: true,
           transparentCorners: false,
         });
-
         canvas.add(rectangle);
         canvas.setActiveObject(rectangle);
+        coordinates_info = {
+          coordinates: canvas._activeObject.lineCoords,
+          left: canvas._activeObject.left,
+          top: canvas._activeObject.top,
+          canvas_width: canvas.width,
+          canvas_height: canvas.height,
+        };
+        setCoord(coordinates_info);
       }
     });
 
@@ -104,8 +101,7 @@ export default function TrainerView(props) {
     });
 
     function deleteObject(eventData, target) {
-      var canvas = target.canvas;
-      canvas.remove(target);
+      canvas.remove(rectangle);
       canvas.requestRenderAll();
     }
 
@@ -134,17 +130,50 @@ export default function TrainerView(props) {
 
     canvas.on("mouse:up", function (o) {
       isDown = false;
+
+      if (canvas._activeObject) {
+        canvas.off("mouse:down");
+      } else {
+        if (
+          canvas._activeObject === null &&
+          canvas._objects.length >= 0 &&
+          canvas._objects.length < 1
+        ) {
+          canvas.on("mouse:down", function (o) {
+            var pointer = canvas.getPointer(o.e);
+            isDown = true;
+            origX = pointer.x;
+            origY = pointer.y;
+            fabric.Object.prototype.transparentCorners = false;
+            fabric.Object.prototype.cornerColor = "red";
+            fabric.Object.prototype.cornerStyle = "circle";
+            rectangle = new fabric.Rect({
+              left: origX,
+              top: origY,
+              fill: "",
+              stroke: "red",
+              strokeWidth: 1,
+              selectable: true,
+              transparentCorners: false,
+            });
+            canvas.add(rectangle);
+            canvas.setActiveObject(rectangle);
+            coordinates_info = {
+              coordinates: canvas._activeObject.lineCoords,
+              left: canvas._activeObject.left,
+              top: canvas._activeObject.top,
+              canvas_width: canvas.width,
+              canvas_height: canvas.height,
+            };
+            setCoord(coordinates_info);
+          });
+        }
+      }
     });
 
     new fabric.Image.fromURL(
       props.nextImage && props.nextImage.data.link,
       function (oImg) {
-        // oImg.set({
-        //   evented: false,
-        // });
-        // oImg.scaleToWidth(canvas.width);
-        // oImg.scaleToWidth(canvas.height);
-        // canvas.add(oImg);
         canvas.setBackgroundImage(oImg, canvas.renderAll.bind(canvas), {
           scaleX: canvas.width / oImg.width,
           scaleY: canvas.height / oImg.height,
@@ -153,23 +182,14 @@ export default function TrainerView(props) {
     );
   };
 
-  // const canvasRef = useRef(null);
-
-  // const draw = (ctx) => {
-  //   ctx.beginPath();
-  //   ctx.rect(20, 20, 150, 100);
-  //   ctx.stroke();
-  // };
-
   useEffect(() => {
     setCanvas(initCanvas(props));
+    setCoord();
   }, [props]);
 
-  // useEffect(() => {
-  //   const canvas = canvasRef.current;
-  //   const context = canvas.getContext("2d");
-  //   draw(context);
-  // }, [draw]);
+  useEffect(() => {
+    setCoord(coord);
+  }, [coord]);
 
   return (
     <div className={classes.root}>
@@ -179,13 +199,6 @@ export default function TrainerView(props) {
         <Grid item xs={12} md={12} lg={12} align="center">
           <Paper className={classes.paper} elevation={0}>
             <canvas id="canvas" {...props} width="400" height="400" />
-            {/* <img
-              id="trainerImage"
-              className={classes.media}
-              src={props.nextImage && props.nextImage.data.link}
-              title="trainer preview"
-              alt="trainer data"
-            /> */}
           </Paper>
         </Grid>
 
@@ -219,6 +232,7 @@ export default function TrainerView(props) {
                             classified: "yes",
                             class: "right",
                             id: props.nextImage && props.nextImage.data.id,
+                            coordinates: coord && coord,
                           },
                         };
                         props.saveData && props.saveData(data);
@@ -230,6 +244,7 @@ export default function TrainerView(props) {
                             classified: "yes",
                             class: "wrong",
                             id: props.nextImage && props.nextImage.data.id,
+                            coordinates: coord && coord,
                           },
                         };
                         props.saveData && props.saveData(data);
@@ -241,6 +256,7 @@ export default function TrainerView(props) {
                             classified: "yes",
                             class: "unrelated",
                             id: props.nextImage && props.nextImage.data.id,
+                            coordinates: coord && coord,
                           },
                         };
                         props.saveData && props.saveData(data);
